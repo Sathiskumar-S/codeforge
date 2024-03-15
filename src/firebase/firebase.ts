15a9244910d58a502f50d0192ac19b3752cca7df
Sathiskumar-S <sathiskumarsaravanan243214@gmail.com>
2024-03-15 15:09:16 +0530

    Update: Firebase Config

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCr4KKgcwzNNkJ_TjqLIU2Z4vYLG26vwgk",
-  authDomain: "codeforge-69a01.firebaseapp.com",
-  projectId: "codeforge-69a01",
-  storageBucket: "codeforge-69a01.appspot.com",
-  messagingSenderId: "308815094605",
-  appId: "1:308815094605:web:d545df2ad9d2515b910d04",
+  apiKey: "AIzaSyAZIDkWc5-Pu2r6HELbfAJdSzkvpOqxeZM",
+  authDomain: "codeforge-7cf2a.firebaseapp.com",
+  projectId: "codeforge-7cf2a",
+  storageBucket: "codeforge-7cf2a.appspot.com",
+  messagingSenderId: "17975894703",
+  appId: "1:17975894703:web:a9c1b257bb817dc6f7ea5a"
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
