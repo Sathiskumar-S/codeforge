815c7c399beebdeb2ca67ed77dd3a05d7b14090c
sathiskumar saravanan <92699503+Sathiskumar-S@users.noreply.github.com>
2024-04-16 11:22:11 +0530

    Update Topbar.tsx

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -45,7 +45,7 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
 		<nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
 			<div className={`flex w-full items-center justify-between ${!problemPage ? "max-w-[1200px] mx-auto" : ""}`}>
 				<Link href='/' className='h-[22px] flex-1'>
-					<Image src='/logo-full.png' alt='Logo' height={100} width={100} />
+					<Image src='/full-logo.png' alt='Logo' height={100} width={100} />
 				</Link>
 
 				{problemPage && (
@@ -75,7 +75,7 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
 				)}
 
 				<div className='flex items-center space-x-4 flex-1 justify-end'>
-					<div>
+{/* 					<div>
 						<a
 							href='https://www.buymeacoffee.com/burakorkmezz'
 							target='_blank'
@@ -84,7 +84,7 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
 						>
 							Premium
 						</a>
-					</div>
+					</div> */}
 					{!user && (
 						<Link
 							href='/auth'
