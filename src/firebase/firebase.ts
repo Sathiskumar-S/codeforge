cf3c8da42363d02302af0e68f05ce5047f1aae4a
Sathiskumar-S <sathiskumarsaravanan243214@gmail.com>
2024-04-16 10:27:50 +0530

    DB config changed

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,13 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZIDkWc5-Pu2r6HELbfAJdSzkvpOqxeZM",
-  authDomain: "codeforge-7cf2a.firebaseapp.com",
-  projectId: "codeforge-7cf2a",
-  storageBucket: "codeforge-7cf2a.appspot.com",
-  messagingSenderId: "17975894703",
-  appId: "1:17975894703:web:a9c1b257bb817dc6f7ea5a"
+  apiKey: "AIzaSyCr4KKgcwzNNkJ_TjqLIU2Z4vYLG26vwgk",
+  authDomain: "codeforge-69a01.firebaseapp.com",
+  databaseURL: "https://codeforge-69a01-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "codeforge-69a01",
+  storageBucket: "codeforge-69a01.appspot.com",
+  messagingSenderId: "308815094605",
+  appId: "1:308815094605:web:d545df2ad9d2515b910d04"
 };
+
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
